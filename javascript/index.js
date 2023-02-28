558ba37ce4f99836e83da8e8da6280cacc5edee2
Matias Posadas <93284124+MatiasPosadas@users.noreply.github.com>
2023-02-28 03:56:52 -0300

    Se agrego el Scroll Reveal

---
+++ javascript/index.js
@@ -0,0 +1,25 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.reveal-down', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    sr.reveal('.reveal-left', {
+        duration: 2500,
+        origin: 'left',
+        distance: '300px'
+    });
+
+    sr.reveal('.reveal-right', {
+        duration: 2500,
+        origin: 'right',
+        distance: '300px'
+    });
+
+    sr.reveal('.reveal-down', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
